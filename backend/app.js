c43a3c58047aae804ef3312da2fbdadf137b48a6
Tushar Panja <panja.tushar15@gmail.com>
2022-06-04 13:35:05 +0530

    gaphql

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const { graphqlHTTP } = require('express-graphql');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -40,11 +43,41 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated!')
+    }
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided!' })
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath)
+    }
+    return res.status(201).json({ message: 'File stored.', filePath: req.file.path  .replace("\\", "/") })
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occured!';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data }
+    }
+}));
 
 app.use((error, req, res, next) => {
     console.log(error)
@@ -56,16 +89,7 @@ app.use((error, req, res, next) => {
 
 mongoose.connect('mongodb+srv://raj:EvlYbfEtt2QJ889N@cluster0.qvv5l.mongodb.net/messages?retryWrites=true&w=majority')
     .then(result => {
-        const server = app.listen(8080);
-        const io = require('./socket').init(server, {
-            cors: {
-                origin: "http://localhost:3000",
-                methods: ["GET", "POST"]
-            }
-        });
-        io.on('connection', socket => {
-            console.log("Client connected");
-        })
+        app.listen(8080);
     })
     .catch(err => {
         console.log(err)
